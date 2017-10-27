e5360a4afea9284bed2292e6ce366c987144d041
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2017-10-27 21:01:03 +0300

    Writer class is made more functional.

--- src/com/scraper/Writer.java
+++ src/com/scraper/Writer.java
@@ -18,16 +18,25 @@ class Writer {
         return fileName.toLowerCase().replace(" ", "_") + ".js";
     }
 
-    void createFiles(String fileName, String instructions, String solution) throws IOException {
+    private String convertPath(String fileName) {
         this.path = this.path.replace("~", System.getProperty("user.home"));    // replace the "~" symbol with home path
-        String convertedFileName = convertFileName(fileName);
-        String fullPath = this.path + File.separator + convertedFileName;   // concatenate path with file name
+        return this.path + File.separator + fileName;   // concatenate path with file name
+    }
+
+    private void makeFiles(String fileName, String instructions, String solution, String fullPath) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(fullPath))) {
             bw.write("/*\n" + instructions + "*/\n\n" + solution + "\n"); // write instructions at the top in comment blocks, then solution
-            System.out.println(String.format("File is successfully created: %s", convertedFileName));
+            System.out.println(String.format("File is successfully created: %s", fileName));
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println(String.format("Error occurred while trying to create: %s", convertedFileName));
+            System.out.println(String.format("Error occurred while trying to create: %s", fileName));
         }
+
+    }
+
+    void createFiles(String fileName, String instructions, String solution) {
+        String convertedFileName = convertFileName(fileName);
+        String fullPath = convertPath(convertedFileName);
+        makeFiles(convertedFileName, instructions, solution, fullPath);
     }
 }
